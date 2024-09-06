7303f8431452e6e83378486134e751e1c33568e2
Dillon <81748327+Wongsodillon@users.noreply.github.com>
2024-09-06 23:42:59 +0700

    Update settings.ts

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://pathera-backend.onrender.com";
-export const FLASK_URL = "https://pathera-ai.onrender.com/";
+export const FLASK_URL = "https://pathera-ai.onrender.com";
